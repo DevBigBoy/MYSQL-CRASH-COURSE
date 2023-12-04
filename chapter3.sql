e3123e0f1642897380472847d9192a8693296f0a
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2023-12-04 05:45:25 +0200

    Add chapter 3 part 2

--- chapter3.sql
+++ chapter3.sql
@@ -62,3 +62,147 @@ select continent_id,
        population
 from   continent
 order by population desc;
+
+--
+-- Formatting SQL Code
+-- 
+
+select continent_id, continent_name, population from continent;
+
+select continent_id, continent_name, population 
+from continent;
+
+-- Both options return the same results
+
+--
+-- Uppercase Keywords
+-- 
+
+SELECT continent_id,
+       continent_name,
+       population
+FROM   continent;
+
+
+use pet;
+
+-- Drop the table (if it exists) before recreating it
+drop table if exists dog;
+
+-- This table depends on the owner, breed, and veterinarian tables existing first.
+-- If you did not create those tables, you can find the SQL in chapter_2.sql
+CREATE TABLE dog
+(
+    dog_id            int,
+    dog_name          varchar(50) UNIQUE,
+    owner_id          int,
+    breed_id          int,
+    veterinarian_id   int,
+    PRIMARY KEY (dog_id),
+    FOREIGN KEY (owner_id) REFERENCES owner(owner_id),
+    FOREIGN KEY (breed_id) REFERENCES breed(breed_id),
+    FOREIGN KEY (veterinarian_id) REFERENCES veterinarian(veterinarian_id)
+);
+
+--
+-- Backticks
+--
+select `continent_id`,
+       `continent_name`,
+       `population`
+from   `continent`;
+
+-- The next 3 SQL statements show comments in SQL:
+
+-- This SQL statement shows the highest-populated continents at the top
+select continent_id,
+       continent_name,
+       population
+from   continent
+order by population desc;
+
+select continent_id,
+       continent_name, -- Continent names are displayed in English
+       population
+from   continent
+order by population desc;
+
+/* 
+This query retrieves data for all the continents in the world. 
+The population of each continent is updated in this table yearly.
+*/
+select * from continent;
+
+select /*+ no_index(employee idx1) */
+    employee_name
+from employee;
+
+--
+-- Null Values
+--
+
+create database employment;
+
+use employment;
+
+create table unemployed
+(
+	region_id   int,
+	unemployed	int
+);
+
+insert into unemployed (region_id, unemployed)
+values 
+(1, 2218457),
+(2, 137455),
+(3, null);
+
+select * 
+from   unemployed
+where  unemployed is null;
+
+select * 
+from   unemployed
+where  unemployed is not null;
+
+--
+-- Try It Yourself Exercises:
+--
+
+-- If you didn't already create the feedback database in chapter 2, let's create it here
+create database if not exists feedback;
+
+-- Make sure the customer table is loaded for the Try It Yourself exercises:
+use feedback;
+
+-- Drop any old versions of this table (if they exist) before recreating the table
+drop table if exists customer;
+
+create table customer
+(
+    customer_id     int,
+    first_name	    varchar(50),
+    last_name       varchar(50),
+    address         varchar(100),
+    primary key (customer_id)
+); 
+
+-- Unless you already did this in chapter 2, load the customer table with data.
+insert into customer (customer_id, first_name, last_name, address)
+values
+(1, 'Bob', 'Smith', '12 Dreary Lane'),
+(2, 'Sally', 'Jones', '76 Boulevard Meugler'),
+(3, 'Karen', 'Bellyacher', '354 Main Street');
+
+-- Exercise 3-1: Select first and last name from the customer table
+select	first_name,
+		last_name
+from	customer;
+
+-- Exercise 3-2: Retrieve the customer_id, first_name, and last_name for customers with a first name of Karen
+select	customer_id,
+		first_name,
+		last_name
+from	customer
+where	first_name = 'Karen';
+
